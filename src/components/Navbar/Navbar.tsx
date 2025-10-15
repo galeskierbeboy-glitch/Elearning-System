e9ff215b317f2a9f5a91d042e3847151362630bd
santiagokerbylhan8-cmd <santiago.kerbylhan8@gmail.com>
2025-10-15 22:43:32 +0800

    Add files via upload

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Navbar.css';
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+// import './Navbar.css';
 
-const Navbar = () => {
-    return (
-        <nav className="navbar">
-            <ul>
-                <li><Link to="/home">Home</Link></li>
-                <li><Link to="/settings">Settings</Link></li>
-                <li><Link to="/my-courses">My Courses</Link></li>
-                <li><Link to="/overview">Overview</Link></li>
-                <li><Link to="/profile">Profile</Link></li>
-                <li><Link to="/">Logout</Link></li>
-            </ul>
-        </nav>
-    );
-};
+// const Navbar = () => {
+//     return (
+//         <nav className="navbar">
+//             <ul>
+//                 <li><Link to="/home">Home</Link></li>
+//                 <li><Link to="/settings">Settings</Link></li>
+//                 <li><Link to="/my-courses">My Courses</Link></li>
+//                 <li><Link to="/overview">Overview</Link></li>
+//                 <li><Link to="/profile">Profile</Link></li>
+//                 <li><Link to="/">Logout</Link></li>
+//             </ul>
+//         </nav>
+//     );
+// };
 
-export default Navbar;
+// export default Navbar;
